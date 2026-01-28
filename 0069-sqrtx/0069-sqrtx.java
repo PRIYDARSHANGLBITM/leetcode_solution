241010386dfc7769d7be9afe647eaf8434a9c837
PRIY DARSHAN <priydarshan12345@gmail.com>
2026-01-29 01:36:29 +0530

    Time: 1 ms (96.59%), Space: 42.5 MB (73.26%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,9 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-        int i=0;
-        while((long)i*i<=x){
-            i++;
+        int left=0;
+        int right = x;
+        int ans =0;
+        while(left<=right){
+            int mid = (left+ (right-left)/2);
+            if((long)mid*mid<=x){
+            ans = mid;
+            left=mid+1;
+        }else{
+            right=mid-1;
         }
-        return i-1;
+        }
+        return ans;
     }
 }
