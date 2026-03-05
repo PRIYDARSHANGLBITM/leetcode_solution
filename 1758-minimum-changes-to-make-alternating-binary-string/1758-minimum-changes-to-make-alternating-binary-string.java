cb5a09d9be2229c2435f8f7b5057ca26549b7289
PRIY DARSHAN <priydarshan12345@gmail.com>
2026-03-05 17:06:51 +0530

    Time: 5 ms (53.53%), Space: 43.3 MB (98.97%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(String s) {
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if((i%2 ==0 && s.charAt(i) != '0')||(i%2 ==1 && s.charAt(i) != '1')){
+                count++;
+            }
+        }
+        return Math.min(count,s.length()-count);
+    }
+}
