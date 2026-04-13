db4bb1c6c3d81e9c4ff10e095905be0ddc568e10
PRIY DARSHAN <priydarshan12345@gmail.com>
2026-04-13 15:09:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,8 +1,8 @@
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+class Solution{
+    public static boolean containsDuplicate(int[]nums){
+        HashSet <Integer> set=new HashSet<>();
         for(int num:nums){
-            if (set.contains(num)){
+            if(set.contains(num)){
                 return true;
             }
             set.add(num);
