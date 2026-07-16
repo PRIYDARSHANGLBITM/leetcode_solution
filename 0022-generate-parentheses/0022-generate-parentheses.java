506ca0af0d50ec3d05b48b82f28ff78096c0946d
PRIY DARSHAN <priydarshan12345@gmail.com>
2026-07-16 23:28:16 +0530

    Time: 2 ms (71.03%), Space: 44.7 MB (56.09%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -4,16 +4,16 @@ class Solution {
         backtrack(result,"",0,0,n);
         return result;
     }
-    private void backtrack(List<String>result, String curr, int open, int close, int n){
+    private void backtrack(List<String>result, String curr, int openbracket, int closebracket, int n){
         if (curr.length() == 2*n ){
             result.add(curr);
             return;
         }
-        if (open<n){
-            backtrack(result,curr+"(",open+1,close,n);
+        if (openbracket<n){
+            backtrack(result,curr+"(",openbracket+1,closebracket,n);
         }
-        if(close < open){
-            backtrack(result,curr+")",open,close+1,n);
+        if(closebracket < openbracket){
+            backtrack(result,curr+")",openbracket,closebracket+1,n);
         }
     }
 }
