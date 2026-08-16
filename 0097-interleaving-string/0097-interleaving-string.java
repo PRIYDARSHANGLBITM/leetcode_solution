5a275792d310d094fa5345d71e53a869f67c2340
PRIY DARSHAN <priydarshan12345@gmail.com>
2026-08-16 21:18:41 +0530

    Time: 4 ms (54.53%), Space: 43.6 MB (17.35%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,29 +1,29 @@
 class Solution {
-    public boolean isInterleave(String s1, String s2, String s3) {
-        int m = s1.length();
-        int n = s2.length();
-        if (m + n != s3.length()) {
+    public boolean isInterleave(String st1, String st2, String st3) {
+        int m = st1.length();
+        int n = st2.length();
+        if (m + n != st3.length()) {
             return false;
         }
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
         for (int i = 1; i <= m; i++) {
             dp[i][0] = dp[i - 1][0] &&
-                       s1.charAt(i - 1) == s3.charAt(i - 1);
+                       st1.charAt(i - 1) == st3.charAt(i - 1);
         }
         for (int j = 1; j <= n; j++) {
             dp[0][j] = dp[0][j - 1] &&
-                       s2.charAt(j - 1) == s3.charAt(j - 1);
+                       st2.charAt(j - 1) == st3.charAt(j - 1);
         }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                boolean fromS1 =
+                boolean fromSt1 =
                         dp[i - 1][j] &&
-                        s1.charAt(i - 1) == s3.charAt(i + j - 1);
-                boolean fromS2 =
+                        st1.charAt(i - 1) == st3.charAt(i + j - 1);
+                boolean fromSt2 =
                         dp[i][j - 1] &&
-                        s2.charAt(j - 1) == s3.charAt(i + j - 1);
-                dp[i][j] = fromS1 || fromS2;
+                        st2.charAt(j - 1) == st3.charAt(i + j - 1);
+                dp[i][j] = fromSt1 || fromSt2;
             }
         }
         return dp[m][n];
