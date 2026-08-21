6996eb2614460be4e4c6031eace62fa9cb649fa5
PRIY DARSHAN <priydarshan12345@gmail.com>
2026-08-21 23:48:02 +0530

    Time: 57 ms (39.26%), Space: 48 MB (24.25%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> result = new ArrayList<>();
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        queue.offer(s);
+        visited.add(s);
+        boolean found = false;
+        while(!queue.isEmpty()){
+            String current = queue.poll();
+            if(isValid(current)){
+                result.add(current);
+                found = true;
+            }
+            if(found){
+                continue;
+            }
+            for(int i=0;i<current.length();i++){
+                if(current.charAt(i)!='('&& current.charAt(i)!=')'){
+                    continue;
+                }
+                String next = current.substring(0, i) + current.substring(i+1);
+                if(!visited.contains(next)){
+                    visited.add(next);
+                    queue.offer(next);
+                }
+            }
+        }
+        return result;
+    }
+    private boolean isValid(String s){
+        int balance = 0;
+        for(char ch: s.toCharArray()){
+            if(ch=='('){
+                balance++;
+            }
+            else if(ch==')'){
+                balance--;
+                if(balance<0){
+                    return false;
+                }
+            }
+        }
+        return balance == 0;
+    }
+}
