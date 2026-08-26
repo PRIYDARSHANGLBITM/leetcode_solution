01a6d711ac181d12f0aa496e665ce4048d1bdfd1
PRIY DARSHAN <priydarshan12345@gmail.com>
2026-08-26 15:37:40 +0530

    Time: 736 ms (6.41%), Space: 0B (100%) - LeetHub

---
+++ 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select * from products where description collate utf8mb3_bin regexp '(^|[^A-Za-z0-9])SN[0-9]{4}-[0-9]{4}([^A-Za-z0-9]|$)'ORDER BY product_id;
