383b79ccdddc2ead138981ae4b4fe18febb7e31d
PRIY DARSHAN <priydarshan12345@gmail.com>
2026-08-31 22:35:49 +0530

    Time: 664 ms (38.92%), Space: 0B (100%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,2 +1,2 @@
-select employee_id from Employees left join salaries using (employee_id) where salary is null union
+select employee_id from Employees left join Salaries using(employee_id) where salary is null union 
 select employee_id from Salaries left join Employees using (employee_id) where name is null order by employee_id;
